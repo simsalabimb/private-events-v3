0255b6cc5927718b22aef739c6a5eeded96bca08
Simeon Revales <simeonrevales@gmail.com>
2020-05-01 12:44:13 -0400

    user mvc and sessions for simple login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create, :welcome]
+
+  def new
+  end
+
+  def login
+  end
+
+  def welcome
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    sessions[:user_id] = @user.id
+    redirect_to '/welcome'
+  end
+
+
+  def page_requires_login
+  end
+  
+
+  private
+
+  def login_params
+    params.require(:login).permit(:username)
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:new, :create]
+
+    def new
+      @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to '/welcome'
+        else
+            render :new
+        end
+    end
+
+    def show
+        
+    end
+
+    private 
+
+    def user_params
+     params.require(:user).permit(:username)
+    end
+    
+
+end
